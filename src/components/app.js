b17b8149d33aef3dfb93d0f95466661ca163934d
Jason Griggs <ForeverMoreFree@gmail.com>
2018-03-27 15:09:08 -0600

    Added header and header styles

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,16 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>This is my updated header!</h1>
+        <div className="backgroundSkew">
+          <div className="madlib-heading">
+          <h1>ForevermoreFree's Mad-Lib</h1>
+            < div className="madlib_subheading">
+              Fill out the fields below and click the generate button<br/>to see the Mad Lib story.
+
+            </div>
+          </div>
+        </div>
+        
       </div>
     );
   }

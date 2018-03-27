4170829428b342a6925805997816243d8c0d278d
Jason Griggs <ForeverMoreFree@gmail.com>
2018-03-27 14:25:21 -0600

    Changed h1 tag in app.js

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>DevCamp React Starter</h1>
+        <h1>This is my updated header!</h1>
       </div>
     );
   }

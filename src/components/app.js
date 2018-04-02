35e521a929fc28b85796ba9aa6b7d023c2a49bc6
Jason Griggs <ForeverMoreFree@gmail.com>
2018-04-02 14:00:22 -0600

    Added React Component

--- src/components/app.js
+++ src/components/app.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
+import MadlibForm from './madlib_form';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <div className="backgroundSkew">
-          <div className="madlib-heading">
-          <h1>ForevermoreFree's Mad-Lib</h1>
-            < div className="madlib_subheading">
-              Fill out the fields below and click the generate button<br/>to see the Mad Lib story.
 
+          <div className="madlib-heading">
+            <h1>Bottega Mad Libs</h1>  
+            <div className="madlib-subheading">
+              Fill out the fields below and click the generate button<br/>to see the Mad Lib story.
             </div>
           </div>
+          <MadlibForm />
+
         </div>
-        
       </div>
     );
   }
